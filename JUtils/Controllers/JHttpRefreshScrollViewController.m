09030dcf183bdf4ff913cc78b5d694a85b74f114
蒋亮 <liang.jiang@gogen.com.cn>
2017-09-20 15:10:42 +0800

    修复了ios11 scrollview safearea的问题

--- JUtils/Controllers/JHttpRefreshScrollViewController.m
+++ JUtils/Controllers/JHttpRefreshScrollViewController.m
@@ -28,6 +28,12 @@
     _containerView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
     _containerView.backgroundColor = [UIColor clearColor];
     
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0
+    if (@available(iOS 11.0, *)) {
+        _containerView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+#endif
+    
     if (![_containerView isKindOfClass:[UIScrollView class]]) {
         NSAssert(NO, @"容器类不是UIScrollView子类");
     }
