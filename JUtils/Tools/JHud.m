893ef4b243557b9d624cf012853e2527abae26c1
蒋亮 <liang.jiang@gogen.com.cn>
2017-09-26 09:51:10 +0800

    hud bug fix

--- JUtils/Tools/JHud.m
+++ JUtils/Tools/JHud.m
@@ -23,8 +23,8 @@ static const CGFloat kImageHeight = 50.0f;
     JHudView *_hudView;
 
     NSTimer *_timer;    
-    BOOL _show; // 是否已经显示
-    BOOL _animating;// 当前hud 是否显示
+//    BOOL _show; // 是否已经显示
+//    BOOL _animating;// 当前hud 是否显示
 }
 
 @end
@@ -67,43 +67,58 @@ static const CGFloat kImageHeight = 50.0f;
 }
 
 - (void)hudShow {
-    if (!_show && !_animating) {
-        _animating = YES;
-        
-        self.hudView.alpha = 0;
-//        _hudView.transform = CGAffineTransformScale(_hudView.transform, 1.4, 1.4);
-        
-        NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut;
-        
-        [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
-//            _hudView.transform = CGAffineTransformScale(_hudView.transform, 1/1.4, 1/1.4);
-            _hudView.alpha = 1;
-            _overlayView.alpha = 1;
-        }completion:^(BOOL finished){
-            _animating = NO;
-            _show = YES;
-        }];
-    }
+//    if (!_show && !_animating) {
+//        _animating = YES;
+//        self.hudView.alpha = 0;
+//        NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut;
+//        
+//        [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
+//            _hudView.alpha = 1;
+//            _overlayView.alpha = 1;
+//        }completion:^(BOOL finished){
+//            _animating = NO;
+//            _show = YES;
+//        }];
+//    }
+    self.hudView.alpha = 0;
+    NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut;
+    
+    [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
+        _hudView.alpha = 1;
+        _overlayView.alpha = 1;
+    }completion:^(BOOL finished){
+    }];
 }
 
 - (void)hudHide {
-    if (_show && !_animating) {
-        [JHud cancelPreviousPerformRequestsWithTarget:self selector:@selector(dismiss) object:nil];
-        
-        _animating = YES;
-        _show = NO;
-        NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseIn;
-        
-        [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
-//            self.hudView.transform = CGAffineTransformScale(_hudView.transform, 0.7, 0.7);
-            _hudView.alpha = 0;
-            _overlayView.alpha = 0;
-        } completion:^(BOOL finished) {
-             _hudView = nil;
-             _show = NO;
-             _animating = NO;
-         }];
-    }
+//    if (_show && !_animating) {
+//        [JHud cancelPreviousPerformRequestsWithTarget:self selector:@selector(dismiss) object:nil];
+//        
+//        _animating = YES;
+//        _show = NO;
+//        NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseIn;
+//        
+//        [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
+////            self.hudView.transform = CGAffineTransformScale(_hudView.transform, 0.7, 0.7);
+//            _hudView.alpha = 0;
+//            _overlayView.alpha = 0;
+//        } completion:^(BOOL finished) {
+//             _hudView = nil;
+//             _show = NO;
+//             _animating = NO;
+//         }];
+//    }
+    [JHud cancelPreviousPerformRequestsWithTarget:self selector:@selector(dismiss) object:nil];
+//    _show = NO;
+    NSUInteger options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseIn;
+    
+    [UIView animateWithDuration:0.15 delay:0 options:options animations:^{
+        _hudView.alpha = 0;
+        _overlayView.alpha = 0;
+    } completion:^(BOOL finished) {
+        _hudView = nil;
+//        _show = NO;
+    }];
 }
 
 
